81c652e91675d027e1a2a3612faccf106cfc2fac
Adelanoumbo <83387048+Adelanoumbo@users.noreply.github.com>
2021-11-15 22:36:26 -0500

    first2 commit

--- market/__init__.py
+++ market/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
@@ -6,5 +6,3 @@ app = application
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
-
-from market import routes
